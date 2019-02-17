d9438c40c5a0dcbbaee15de0f807c8f93d0efef1
wechris <weiss_christian@web.de>
2019-02-17 22:51:54 +0100

    switched to SapMachine OpenJDK

--- install/gnome.sh
+++ install/gnome.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 echo "install Gnome..."
 sudo zypper ar obs://GNOME:/STABLE:/3.16/openSUSE_13.2/GNOME:STABLE:3.16
 sudo zypper mr -p 98 -r GNOME:STABLE:3.16
@@ -11,11 +12,23 @@ sudo ln -svf /usr/lib/systemd/system/graphical.target /etc/systemd/system/defaul
 sudo chkstat --system --set
 
 echo "install JAVA..."
-wget --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/8u191-b12/2787e4a523244c269598db4e85c51e0c/jdk-8u191-linux-x64.rpm
-sudo zypper --non-interactive remove --force-resolution 'java*'
-sudo ln -s /usr/sbin/update-alternatives /usr/sbin/alternatives
-sudo rpm -ivh --nodeps /home/vagrant/jdk-8u191-linux-x64.rpm
- 
-sudo alternatives --install "/usr/bin/java" "java" "/usr/java/jdk1.8.0_191-amd64/bin/java" 1
-sudo update-alternatives --set java /usr/java/jdk1.8.0_191-amd64/bin/java
 
+  wget "https://github.com/SAP/SapMachine/releases/download/sapmachine-11.0.2/sapmachine-jdk-11.0.2_linux-x64_bin.tar.gz" -O /home/vagrant/sapmachine-jdk-11.0.2_linux-x64_bin.tar.gz && touch java.is.done
+if [ ! -d /usr/java ]; then
+  sudo mkdir /usr/java
+fi
+sudo tar -xzf /home/vagrant/sapmachine-jdk-11.0.2_linux-x64_bin.tar.gz -C /usr/java
+ 
+sudo ln -s /usr/sbin/update-alternatives /usr/sbin/alternatives
+sudo alternatives --install "/usr/bin/java" "java" "/usr/java/sapmachine-11.0.2/bin/java" 1
+sudo update-alternatives --set java /usr/java/sapmachine-11.0.2/bin/java
+ 
+wget "https://download2.gluonhq.com/openjfx/11.0.2/openjfx-11.0.2_linux-x64_bin-jmods.zip" -O /home/vagrant/openjfx-11.0.2_linux-x64_bin-jmods.zip && touch fx.is.done
+sudo unzip /home/vagrant/openjfx-11.0.2_linux-x64_bin-jmods.zip -d /home/vagrant/
+sudo cp -r /home/vagrant/javafx-jmods-11.0.2/* /usr/java/sapmachine-11.0.2/jmods
+sudo touch /usr/java/sapmachine-11.0.2/lib/javafx.properties
+
+#Cleanup
+sudo rm -rf /home/vagrant/sapmachine-jdk-11.0.2_linux-x64_bin.tar.gz
+sudo rm -rf /home/vagrant/openjfx-11.0.2_linux-x64_bin-jmods.zip
+sudo rm -rf /home/vagrant/javafx-jmods-11.0.2
